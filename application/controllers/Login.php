2c9c54afea63cba6a2d4a156cce9fb81ff630e5b
drkidal <drkidal@bitbucket.org>
2020-06-14 16:07:22 +0800

    commit ketiga: halaman login dan dashboard

---
+++ application/controllers/Login.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+
+        //load model CekLogin
+        $this->load->model('ceklogin');
+    }
+
+    function index()
+    {
+        if ($this->ceklogin->is_logged_in()) {
+            //jika memang session sudah terdaftar, maka redirect berdasarkan level user
+            if ($this->session->userdata("level") == "admin") {
+                redirect('admin/dashboard');
+            } else {
+                redirect('pelanggan/dashboard');
+            }
+        } else {
+
+            //jika session belum terdaftar
+            //set form validation
+            $this->form_validation->set_rules('username', 'Username', 'required');
+            $this->form_validation->set_rules('password', 'Password', 'required');
+
+            //set message form validation
+            $this->form_validation->set_message('required', '<div class="alert alert-danger" style="margin-top: 3px">
+                    <div class="header"><b><i class="fa fa-exclamation-circle"></i> {field}</b> harus diisi</div></div>');
+
+            //cek validasi
+            if ($this->form_validation->run() == TRUE) {
+
+                //get data dari FORM
+                $username = $this->input->post("username", TRUE);
+                $password = MD5($this->input->post('password', TRUE));
+
+                //checking data via model
+                $checking = $this->ceklogin->check_login('user', array('username' => $username), array('password' => $password));
+
+                //jika ditemukan, maka create session
+                if ($checking != FALSE) {
+                    foreach ($checking as $apps) {
+
+                        $session_data = array(
+                            'username'  => $apps->username,
+                            'nama'      => $apps->nama,
+                            'level'     => $apps->level
+                        );
+                        //set session userdata
+                        $this->session->set_userdata($session_data);
+
+                        //redirect berdasarkan level user
+                        if ($this->session->userdata("level") == "admin") {
+                            redirect('admin/dashboard');
+                        } else {
+                            redirect('pelanggan/dashboard');
+                        }
+                    }
+                } else {
+
+                    $data['error'] = '<div class="alert alert-danger" style="margin-top: 3px">
+                        <div class="header"><b><i class="fa fa-exclamation-circle"></i> ERROR</b> username atau password salah!</div></div>';
+                    $this->load->view('login', $data);
+                }
+            } else {
+
+                $this->load->view('login');
+            }
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect('login');
+    }
+}
---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        //load model CekLogin
+        $this->load->model('ceklogin');
+        //cek session dan level user
+        if ($this->ceklogin->is_role() != "admin") {
+            redirect('login');
+        }
+    }
+
+    function index()
+    {
+
+        $this->load->view('admin/dashboard');
+    }
+}
